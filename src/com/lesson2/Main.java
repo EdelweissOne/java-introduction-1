c935656cb5e64fa61e0e9f4af180cbc21450daa1
Alena <edelweisone@gmail.com>
2023-07-27 17:15:14 +0300

    first commit

---
+++ src/com/lesson2/Main.java
@@ -0,0 +1,14 @@
+package com.lesson2;
+
+public class Main {
+    /*  многострочнный комментарий
+    * Ctrl+Alt+L   -   fomat the code*/
+    // psvm - creat main method
+    public static void main(String[] args) {
+    // sout - print to console
+        System.out.print("Hello in Idea");
+        System.out.print(" the some line");
+        System.out.println();
+        System.out.println("row 2");
+    }
+}
