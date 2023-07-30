6ec159e20e271184dff129f9b412f2885c5b1c8a
Alena <edelweisone@gmail.com>
2023-07-30 17:46:19 +0300

    lesson 3

---
+++ src/com/hillel/lesson3/TypeCasting.java
@@ -0,0 +1,16 @@
+package com.hillel.lesson3;
+
+public class TypeCasting {
+
+    public static void main(String[] args) {
+        int s = 10;
+        int t = 12;
+
+        float result = (float) s/t;
+
+        System.out.println(result + " per hour");
+    }
+
+
+
+}
