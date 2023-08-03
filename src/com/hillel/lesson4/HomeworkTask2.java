e3ba45ea91ef6f97ae981a4413dd9901e1b0f8ab
Alena <edelweisone@gmail.com>
2023-08-03 18:21:01 +0300

    lesson 4 home task 2

--- src/com/hillel/lesson4/HomeworkTask2.java
+++ src/com/hillel/lesson4/HomeworkTask2.java
@@ -10,23 +10,26 @@ public class HomeworkTask2 {
         int portionCost = 2;
         int onsens = 3;
 
-        int calculatedFoodCat = calculetFoodMonth(cat, portion, onsens);
-        int calculatedFoodDog = calculetFoodMonth(dog, portion, onsens);
 
-        System.out.println(" Корма для котов в месяц : " + calculatedFoodCat);
-        System.out.println(" Корма для собак в месяц : " + calculatedFoodDog);
+        int calculatedCatFoodMonth = calculatedFoodMonth(cat, portion, onsens );
+        System.out.println(" Корма для котов в месяц : " + calculatedCatFoodMonth);
+        int calculetdDogFoodMonth = calculatedFoodMonth(dog, portion, onsens);
+        System.out.println(" Корма для собак в месяц : " + calculetdDogFoodMonth);
 
-        int calculatedCostCat = calculetCostFood(calculatedFoodCat, portionCost, portion);
-        int calculatedCostDog = calculetCostFood(calculatedFoodDog, portionCost, portion);
-
-        System.out.println(" Сумма в месяц на содержание : " + (calculatedCostCat + calculatedCostDog));
+        int calculatedPetAllFood = calculatedAll(calculatedCatFoodMonth,  calculetdDogFoodMonth);
+        int calculatedPetCostFood = calculatedCostFood(calculatedPetAllFood, portionCost, portion);
+        System.out.println(" Сумма в месяц на содержание : " + calculatedPetCostFood);
     }
+    public static int calculatedAll(int calculatedCostCat, int calculatedCostDog) {
 
-    public static int calculetFoodMonth(int pet, int portion, int onsens) {
+        return (calculatedCostCat + calculatedCostDog);
+
+    }
+    public static int calculatedFoodMonth(int pet, int portion, int onsens) {
+
         return (pet * (portion * onsens)) * 30;
     }
-
-    public static int calculetCostFood(int countFood, int portionCost, int portionCount) {
+    public static int calculatedCostFood(int countFood, int portionCost, int portionCount) {
         return (countFood / portionCount) * portionCost;
     }
 
